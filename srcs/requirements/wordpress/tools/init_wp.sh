21d4aa458cca63a718f2211006e2bc146ea5b244
root <root@inception.alemarch.42.fr>
2022-09-06 09:23:02 -0400

    everything running !

--- srcs/requirements/wordpress/tools/init_wp.sh
+++ srcs/requirements/wordpress/tools/init_wp.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-sleep 15
-wp core download --path="/var/www/html/"
-wp config create --path="/var/www/html" --dbname="$WP_DB_NAME"\
-	--dbhost="$WP_DB_HOST" --dbuser="$WP_DB_USER" --dbpass="$WP_DB_PASSWORD"
-wp core install --url="$DOMAIN_NAME" --title="$WP_TITLE" \
-	--admin_name="$WP_ADMIN" --admin_email="alemarch@student.42.fr" --skip-email \
-	--path="/var/www/html/" --admin_password="$WP_ADMIN_PASSWORD" --allow-root
-echo "runnning php-fpm" && php-fpm8 -FR
+echo 'mysqld is not ready yet..'
+sleep 20
+
+if [ ! -f "index.php" ]
+then
+	echo CONFIGURING PHP-FPM
+	echo "listen = 0.0.0.0:9000" >> /etc/php8/php-fpm.d/www.conf
+	sed -i "s/;error_log/error_log/" /etc/php8/php-fpm.conf
+	ln -sf /dev/stderr /var/log/php8/error.log
+	echo DOWNLOADING WORDPRESS
+	wp core download --path="/var/www/html"
+	echo CREATING WORDPRESS CONFIG
+	wp config create --dbname="$WP_DB_NAME" \
+		--dbhost="$WP_DB_HOST" --dbuser="$WP_DB_USER" --dbpass="$WP_DB_PASSWORD"
+	echo INSTALLING WORDPRESS
+	wp core install --url="alemarch.42.fr" --title="$WP_TITLE" \
+		--admin_name="$WP_ADMIN" --admin_email="alemarch@student.42.fr" --skip-email \
+		--admin_password="$WP_ADMIN_PASSWORD" --allow-root
+fi
+echo "running php-fpm" && cat /etc/php8/php-fpm.conf | grep log && php-fpm8 -FR
